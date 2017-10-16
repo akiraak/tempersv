4f3165a6e180325428cb9f43b65fcdcb3786c8ce
Akira Kozakai <akiraak@gmail.com>
2017-10-15 18:38:17 -0700

    Exec command

--- server.go
+++ server.go
@@ -1,15 +1,24 @@
 package main
 
 import (
-	"net/http"
-
+	"fmt"
 	"github.com/labstack/echo"
+	"net/http"
+	"os/exec"
 )
 
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		cmd := exec.Command("date")
+		cmdoutput, err := cmd.Output()
+		output := string(cmdoutput)
+		if err != nil {
+			output = fmt.Sprintf("%s\n%s", output, err)
+		}
+		fmt.Println(output)
+
+		return c.String(http.StatusOK, output)
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
